88a18269dbf02f6088878e0b98bd44a62c703bc3
Mostafa Alsabagh <mostafa.alsabagh@datagearbi.com>
2020-06-26 10:52:58 +0200

    Connect to Firebase  and add file usage xml view.

--- app/src/main/java/com/example/farmclientapplic/MainActivity.java
+++ app/src/main/java/com/example/farmclientapplic/MainActivity.java
@@ -2,15 +2,32 @@ package com.example.farmclientapplic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class MainActivity extends AppCompatActivity {
 
 
+    private Button buttonUpload , buttonChooseFile;
+    private Button buttonDownload;
+    private TextView filenametxt , fileDatatxt;
+    private Uri filePath;
+    private String path;
+    FirebaseStorage storage;
+    StorageReference storageReference;
+    String fileName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        
+
     }
 }

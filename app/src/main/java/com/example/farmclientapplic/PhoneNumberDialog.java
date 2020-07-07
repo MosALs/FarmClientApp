a9b747d385aeabc864c59fbc035c85771b3e60db
Mostafa Alsabagh <mostafa.alsabagh@datagearbi.com>
2020-07-07 21:05:51 +0200

    Add phone number dialog.

---
+++ app/src/main/java/com/example/farmclientapplic/PhoneNumberDialog.java
@@ -0,0 +1,54 @@
+package com.example.farmclientapplic;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatDialogFragment;
+
+public class PhoneNumberDialog extends AppCompatDialogFragment {
+
+    private EditText phoneNumberET;
+    private ExampleDialogListener listener;
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.phoneno_dialog, null);
+        phoneNumberET = view.findViewById(R.id.phoneno_et);
+        builder.setView(view)
+                .setTitle("Phone Number")
+                .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                    }
+                })
+                .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        String phoneNumber = phoneNumberET.getText().toString();
+                        listener.applyTexts(phoneNumber);
+                    }
+                });
+        return builder.create();
+    }
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        try {
+            listener = (ExampleDialogListener) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString() +
+                    "must implement ExampleDialogListener");
+        }
+    }
+    public interface ExampleDialogListener {
+        void applyTexts(String phoneNumber);
+    }
+}
